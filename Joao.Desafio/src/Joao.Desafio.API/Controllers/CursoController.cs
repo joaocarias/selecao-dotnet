1ed5fdf402fd5c415cbcc351bcb7b924997e7ab4
João Carias de França <joaocariasdefranca@gmail.com>
2022-01-04 22:05:12 -0300

    add AppContexto

---
+++ Joao.Desafio/src/Joao.Desafio.API/Controllers/CursoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Joao.Desafio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Novo()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Joao.Desafio/src/Joao.Desafio.Infraestrutura/Contextos/AppContexto.cs
@@ -0,0 +1,18 @@
+﻿using Joao.Desafio.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Joao.Desafio.Infraestrutura.Contextos
+{
+    public class AppContexto : DbContext
+    {
+        public AppContexto(DbContextOptions<AppContexto> options) : base(options) { }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<CartaoCredito> CartaoCreditos { get; set; }
+        public DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+       
+    }
+}
